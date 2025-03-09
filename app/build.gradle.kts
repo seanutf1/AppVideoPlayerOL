cfc3a8d4c38c67c1a8b7b9c6ef6a346e3af5aa4c
seanutf1 <1716534188@qq.com>
2025-03-09 10:36:29 +0800

    init project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -42,6 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.github.CarGuo.GSYVideoPlayer:gsyvideoplayer:v10.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,12 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            setUrl("https://jitpack.io")
+        }
+        maven {
+            setUrl("https://maven.aliyun.com/repository/public")
+        }
     }
 }
 
